6e8341e58b1abd229a8ffa2e9d3abb18ebfb2520
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-13 17:58:16 +0100

    onclick created

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tile = ({ tile }) => {
+  const [showWebcam, setShowWebcam] = useState(false);
   const { type, room } = tile;
-  return <div className={room ? `${room} ${type}` : type}></div>;
+
+  const handleClick = (event) => {
+    if(event.target.value.includes('seat')){
+      setShowWebcam(!showWebcam)
+    }
+  }
+
+  return (
+    <div
+      className={room ? `${room} ${type}` : type}
+      onClick={(event) => {
+        handleClick(event);
+      }}
+    ></div>
+  );
 };
 
 export default Tile;
