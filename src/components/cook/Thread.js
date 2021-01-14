33a505b2e3333eafb31168ff9317fc2c3ef3c728
Aymeric <aymeric.bouault@gmail.com>
2021-01-14 17:18:20 +0100

    map pour le rendue + creation composant card

--- src/components/cook/Thread.js
+++ src/components/cook/Thread.js
@@ -1,10 +1,13 @@
+import { Card } from '@material-ui/core';
 import React, {useState, useEffect} from 'react'
-import { useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPosts } from './PostActions'
+import { isEmpty } from "./utils";
 
 const Thread = () => {
     const [loadPost, setLoadPost] = useState(true);
     const dispatch = useDispatch();
+    const posts = useSelector(state => state.postReducer)
 
     useEffect(()=>{
         if (loadPost){
@@ -14,7 +17,15 @@ const Thread = () => {
     },[loadPost, dispatch])
 
     return(
-        <div>Thread</div>
+        <div className="thread-container">
+            <ul>
+                {!isEmpty(posts[0]) &&
+                    posts.map((post) => {
+                        return <Card post={post} key={post.id}/>
+                    })
+                }
+            </ul>
+        </div>
     )
 }
 
