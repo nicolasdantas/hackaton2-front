d86d95b7a5aadd936cb35b9c70c30ea1c4d49af2
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-14 18:18:51 +0100

    player done

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -8,18 +8,28 @@ const AudioPlayer = () => {
   console.log(musicSelected);
 
   const options = [
-    { value: "chocolate", label: "Chocolate", url: "https://soundcloud.com/glennmorrison/beethoven-moonlight-sonata" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
+    { value: "chill", label: "Chill", url: "https://youtu.be/ujHcGRIXEzU" },
+    { value: "rock", label: "Rock", url: "https://youtu.be/nwYAt3-IPHY" },
+    { value: "trance", label: "Trance", url: "https://youtu.be/WYetg3AuLE4" },
+    {
+      value: "classic",
+      label: "Classic",
+      url: "https://www.youtube.com/watch?v=RVfNygw10NU",
+    },
   ];
 
   return (
-    <section>
-      <Select
-        options={options}
-        onChange={(value) => setMusicSelected(value.url)}
-      />
-      {musicSelected && <ReactPlayer url={musicSelected} />}
+    <section style={{ width: "35%" }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Choose a kind of music !
+      </h1>
+      <div style={{ marginBottom: "20px" }}>
+        <Select
+          options={options}
+          onChange={(value) => setMusicSelected(value.url)}
+        />
+      </div>
+      {musicSelected && <ReactPlayer url={musicSelected} controls={true} />}
     </section>
   );
 };
