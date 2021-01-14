1dd154fc3db7aaf942218ce4ad942d8f4c239045
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-14 05:36:27 +0100

    added an avatar and a tool tip for each user

---
+++ src/components/Avatar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "absolute",
+    zIndex: 1,
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ImageAvatar({ image }) {
+  const classes = useStyles();
+  console.log(image);
+  return (
+    <div className={classes.root}>
+      <Avatar alt={image} src={image} />
+    </div>
+  );
+}
