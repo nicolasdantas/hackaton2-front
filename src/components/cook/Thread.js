2ccc7aedab1a41661c7ac21b57e4d2f8cda4c7a5
Aymeric <aymeric.bouault@gmail.com>
2021-01-14 16:41:00 +0100

    useEffect pour implementer le store

--- src/components/cook/Thread.js
+++ src/components/cook/Thread.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch} from 'react-redux'
+import { getPosts } from './PostActions'
 
 const Thread = () => {
+    const [loadPost, setLoadPost] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        if (loadPost){
+            dispatch(getPosts())
+            setLoadPost(false)
+        }
+    },[loadPost, dispatch])
+
     return(
         <div>Thread</div>
     )
