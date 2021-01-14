c904ea89d97aef89d55a99c58629d8eb3c4d7347
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-14 03:38:28 +0100

    user can now move on grass and every seats

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,18 +3,22 @@ import React, { useState, useContext } from "react";
 import { LoginContext } from "../components/contexts/LoginContext";
 
 const Tile = ({ tile, setShowWebcam, showWebcam }) => {
-
   const { type, room } = tile;
   const { userLogged, setUserLogged } = useContext(LoginContext);
-  
+
   const handleClick = (event) => {
+    if (event.target.className.includes("seat") || event.target.className.includes("grass")) {
+      setUserLogged({
+        ...userLogged,
+        coordX: tile.coordX,
+        coordY: tile.coordY,
+      });
+    }
     if (
       event.target.className.includes("space") &&
       event.target.className.includes("seat")
     ) {
       setShowWebcam(true);
-      setUserLogged({...userLogged, coordX: tile.coordX, coordY:tile.coordY});
-      console.log(userLogged)
     }
   };
 
