ffcabcdedaf8efd168c7bd1fc59f8e0d233cb636
Aymeric <aymeric.bouault@gmail.com>
2021-01-15 00:23:20 +0100

    recuperation des data pour les recettes

--- src/components/cook/Actu.js
+++ src/components/cook/Actu.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import './Actu.scss'
+import { useToasts } from 'react-toast-notifications';
 
 export default function Actu() {
-
+    const { addToast } = useToasts();
     const [postRecipe, setPostRecipe] = useState([])
 
     const [cookName, setCookname] = useState()
@@ -17,14 +18,18 @@ useEffect(() => {
     })
 },[])
 
-/* const handleSubmit = () => {
-    axios.post('http://8ded0b75d1f4.ngrok.io/api/users')
+const handleSubmit = (data) => {
+    axios.post('http://8ded0b75d1f4.ngrok.io/api/users', data)
     .then((res) => setPostRecipe(res.data))
-} */
+    addToast('Votre contenu a bien été publier', {
+        appearance: 'success',
+        autoDismiss: true,
+      });
+}
 
     return (
         <div className="container-page-actu">
-        <form className="container-form-actu" /* onSubmit={handleSubmit} */>
+        <form className="container-form-actu" onSubmit={handleSubmit}>
         <h2 className="title-recette">Publie ta Recette !</h2>
         <div className="form-post-actu">
         <input
@@ -58,13 +63,20 @@ useEffect(() => {
                 postRecipe.map((post) => {
                     return (
                         <div className="cards-container" key={post.id}>
+                            <div className="profil-user">
                             <h3>{post.username}</h3>
-                            <img className="image-recipe" src={post.avatar} alt={post.username} />
-                            <p>{post.lunches.name}</p>
-                            <img src={post.lunches.image} alt={post.lunches.name} />
-                            <p>{post.lunches.rate}</p>
-                            <p>{post.lunches.content}</p>
-                            <p>{post.lunches.name}</p>
+                            <img className="image-avatar" src={post.avatar} alt={post.username} />
+                            </div>
+                            {post.lunches.length > 0 &&
+                            <div className="infos-recipes">
+                            <div className="name-date">
+                                <h3>{post.lunches[0].name}</h3>
+                                <p>{post.lunches[0].created_at}</p>
+                            </div>
+                            <img className="image-recipe" src={post.lunches[0].image} alt={post.lunches[0].name} />
+                            <p>{post.lunches[0].content}</p>
+                            <p>note: {post.lunches[0].rate}</p>
+                            </div>}
                         </div>
                     )
                 })
